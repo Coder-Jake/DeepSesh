a7711afbecc3aaf96621cbe53bf0ee6f62253b6b
[dyad] <hire@jacobv.xyz>
2025-08-28 16:19:35 +1000

    Updated src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -112,7 +112,7 @@ const Index = () => {
       setIsRunning(false);
       setIsPaused(false);
       setIsFlashing(false);
-      const initialTime = timerType === 'DeepWork' ? focusMinutes * 60 : breakMinutes * 60;
+      const initialTime = timerType === 'focus' ? focusMinutes * 60 : breakMinutes * 60;
       setTimeLeft(initialTime);
     } else {
       if (confirm('Are you sure you want to reset the timer?')) {
