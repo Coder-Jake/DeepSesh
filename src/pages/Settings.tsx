4b9dd5310dc83fd9c09298f0cd49880562f74964
[dyad] <hire@jacobv.xyz>
2025-08-28 22:19:41 +1000

    Updated src/pages/Settings.tsx

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -283,11 +283,11 @@ const Settings = () => {
                   <SelectValue placeholder="Select duration" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="5">5 minutes</SelectItem>
-                  <SelectItem value="10">10 minutes</SelectItem>
-                  <SelectItem value="15">15 minutes</SelectItem>
-                  <SelectItem value="20">20 minutes</SelectItem>
-                  <SelectItem value="30">30 minutes</SelectItem>
+                  <SelectItem value="5">5 mins</SelectItem>
+                  <SelectItem value="10">10 mins</SelectItem>
+                  <SelectItem value="15">15 mins</SelectItem>
+                  <SelectItem value="20">20 mins</SelectItem>
+                  <SelectItem value="30">30 min</SelectItem>
                 </SelectContent>
               </Select>
             </div>
