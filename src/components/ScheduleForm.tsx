415873ff7d97840a8ea054d9b5a9e363525863d6
[dyad] <hire@jacobv.xyz>
2025-09-29 18:17:43 +1000

    Updated src/components/ScheduleForm.tsx

--- src/components/ScheduleForm.tsx
+++ src/components/ScheduleForm.tsx
@@ -148,7 +148,6 @@ const ScheduleForm: React.FC = () => {
             />
           </div>
           <div className="space-y-2">
-            <Label htmlFor="commence-day">Commence</Label>
             <Select value={commenceDay.toString()} onValueChange={(value) => setCommenceDay(parseInt(value))}>
               <SelectTrigger id="commence-day">
                 <SelectValue placeholder="Select day" />
