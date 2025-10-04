c9a2465b9b8b6b3d5aded0561db10562b3649b59
[dyad] <hire@jacobv.xyz>
2025-10-05 10:21:51 +1100

    Updated src/components/ScheduleForm.tsx

--- src/components/ScheduleForm.tsx
+++ src/components/ScheduleForm.tsx
@@ -317,7 +317,7 @@ const ScheduleForm: React.FC = () => {
           </Button>
         </CardHeader>
         <TabsContent value="plan" className="pt-0 pb-6 space-y-2 px-4 lg:px-6" id="plan-tab-content">
-          <div className="space-y-0 max-h-[400px] overflow-y-auto pr-2">
+          <div className="space-y-1 max-h-[400px] overflow-y-auto pr-2">
             {schedule.map((timer, index) => (
               <div 
                 key={timer.id} 
