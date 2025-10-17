e4105ecf6b1924af47b3c82fe0866055e087b895
[dyad] <hire@jacobv.xyz>
2025-10-17 14:58:18 +1100

    [dyad] Adjusted tooltip delay for mobile - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -219,7 +219,7 @@ const AppContent = () => {
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider delayDuration={0} skipDelayDuration={300}> {/* Increased skipDelayDuration */}
+    <TooltipProvider delayDuration={700}> {/* Adjusted delayDuration */}
       <ThemeProvider>
         <AuthProvider>
           <ProfileProvider>
