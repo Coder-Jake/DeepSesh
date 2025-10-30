e8407b928054ed3c54b95c19416fe279c446b165
[dyad] <hire@jacobv.xyz>
2025-10-31 10:37:56 +1100

    [dyad] Adjusting logo vertical position - wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -45,7 +45,7 @@ const Header = () => {
             <img 
               src="/Deepsesh Logo - size 3.png" 
               alt="DeepSesh Logo" 
-              className="h-8 w-8 mr-0 mt-[-2px]" // Changed margin-right to mr-0
+              className="h-8 w-8 mr-0 mt-[-4px]" // Adjusted margin-top from mt-[-2px] to mt-[-4px]
             />
             <h1 className="text-3xl font-bold select-none bg-gradient-to-r from-blue-500 to-purple-600 text-transparent bg-clip-text">
               DeepSesh
