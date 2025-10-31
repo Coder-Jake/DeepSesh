8cf3b1e86f02b3ff13a94baf8d8d329366bf136b
[dyad] <hire@jacobv.xyz>
2025-11-01 07:35:01 +1100

    [dyad] Fixing import error for main page component - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { TooltipProvider } from '@/components/ui/tooltip'; // Import TooltipProvider
-import HomePage from './pages/HomePage'; // Assuming you have a HomePage component
-import './index.css'; // Assuming your global styles are here
+import { TooltipProvider } from '@/components/ui/tooltip';
+import Index from './pages/Index'; // Corrected import path and component name
+import './index.css';
 
 function App() {
   return (
-    <TooltipProvider> {/* Wrap the entire application with TooltipProvider */}
+    <TooltipProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Index />} /> {/* Using the corrected component name */}
           {/* Add other routes here as needed */}
         </Routes>
       </Router>
