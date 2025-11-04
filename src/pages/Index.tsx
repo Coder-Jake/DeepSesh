c26d06cb2a0477671bb61856796de9294c44c85d
[dyad] <hire@jacobv.xyz>
2025-11-05 08:55:51 +1100

    [dyad] wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1085,7 +1085,7 @@ const Index = () => {
         return a.title.localeCompare(b.title);
       }
       return timeA - timeB;
-    );
+    }); // Closing brace added here
   }, [preparedSchedules, getEffectiveStartTime]);
 
   const handleNameClick = useCallback(async (userId: string, userName: string, event: React.MouseEvent) => {
