558659f2b3d081386a0589df4710f28872e188a4
[dyad] <hire@jacobv.xyz>
2025-11-06 10:13:53 +1100

    [dyad] Moved AskMenu button below the timer - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1345,6 +1345,12 @@ const Index = () => {
                     </CircularProgress>
                   </div>
                   
+                  {(isRunning || isPaused || isScheduleActive || isSchedulePrepared || isSchedulePending) && (
+                    <div className="flex justify-center mb-6"> {/* Added div for spacing */}
+                      <AskMenu onExtendSubmit={handleExtendSubmit} onPollSubmit={handlePollSubmit} />
+                    </div>
+                  )}
+
                   <div className="flex gap-3 justify-center mb-6">
                     {isFlashing ? (
                       <Button 
@@ -1480,7 +1486,6 @@ const Index = () => {
                       </div>
                     </div>
                   )}
-                  {(isRunning || isPaused || isScheduleActive || isSchedulePrepared || isSchedulePending) && <AskMenu onExtendSubmit={handleExtendSubmit} onPollSubmit={handlePollSubmit} />}
                 </>
               )}
             </div>
