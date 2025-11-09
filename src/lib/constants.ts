1b4ac0f4e6dd9aac1ec5da8260c172e40ca8871a
[dyad] <hire@jacobv.xyz>
2025-11-09 21:14:02 +1100

    [dyad] Moved host code generation dependencies to constants and exported the function. - wrote 2 file(s)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,15 @@
 export const DAYS_OF_WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+export const colors = [
+  "red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "grey", "black", "white",
+  "gold", "silver", "bronze", "indigo", "violet", "teal", "cyan", "magenta", "lime", "maroon",
+  "navy", "olive", "aqua", "fuchsia", "azure", "beige", "coral", "crimson", "lavender", "plum"
+];
+export const animals = [
+  "fox", "bear", "cat", "dog", "lion", "tiger", "wolf", "deer", "zebra", "panda", "koala",
+  "eagle", "hawk", "owl", "duck", "swan", "robin", "sparrow", "penguin", "parrot", "flamingo",
+  "shark", "whale", "dolphin", "octopus", "squid", "crab", "lobster", "jellyfish", "starfish",
+  "snake", "lizard", "frog", "toad", "turtle", "snail", "spider", "bee", "ant", "butterfly",
+  "elephant", "giraffe", "monkey", "gorilla", "chimpanzee", "hippopotamus", "rhinoceros", "camel",
+  "horse", "cow", "pig", "sheep", "goat", "chicken", "rabbit", "mouse", "rat", "squirrel", "badger"
+];
