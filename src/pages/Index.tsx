b2e81ed481b365dccca7729a9e260e2e107254f5
[dyad] <hire@jacobv.xyz>
2025-12-03 14:36:16 +1100

    Updated src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -106,7 +106,7 @@ interface SupabaseSessionData {
   join_code: string | null;
   organisation: string[] | null; // MODIFIED: Changed to string[] | null
   host_notes: string | null;
-  active_asks: ActiveAskItem[]; // NEW: Added active_asks property
+  active_asks: ActiveAskItem[];
   is_mock: boolean; // NEW: Added is_mock property
 }
 
