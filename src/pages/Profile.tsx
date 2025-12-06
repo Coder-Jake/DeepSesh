ea281f5eec73dd4b382dcde8e0dc1c2aaf66a32f
[dyad] <hire@jacobv.xyz>
2025-12-06 13:31:00 +1100

    [dyad] Fixed unclosed div tag in Profile page - wrote 1 file(s)

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -922,6 +922,7 @@ const Profile = () => {
                     {focusPreferenceInput > 80 && "Minimal interaction even during breaks"}
                   </div>
                 </div>
+              </div>
             </CardContent>
           </Card>
 
